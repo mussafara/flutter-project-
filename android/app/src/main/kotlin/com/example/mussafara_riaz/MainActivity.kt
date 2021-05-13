590b45663e0bf82af5c3f681ccb9a353a1a6a397
mussafara riaz <mussafarariaz@gmail.com>
2021-05-13 13:07:47 -0700

    this is my first app which i learn from jawan pakistan

---
+++ android/app/src/main/kotlin/com/example/mussafara_riaz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mussafara_riaz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text("MUSSAFARA RIAZ HERE"),
+      )
+
+
+      
+    );
+  }
+}
